1c2bf4c83b1338755a3797222dab17fae07ea9b1
Breno Amin <brenomatos125@gmail.com>
2023-11-04 11:17:20 -0300

    first-commit(feature): create react+vite project with the first component(ProcessBox)

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import { ProcessBox } from "./components/ProcessBox";
+
+function App() {
+  return <ProcessBox/>;
+}
+
+export default App;
---
+++ src/components/ProcessBox/index.tsx
@@ -0,0 +1,80 @@
+import { ReactElement } from "react";
+import close from "../../assets/close-button.svg";
+import "./index.css";
+
+export const ProcessBox = (): ReactElement => {
+  return (
+    <div className="form-wrapper">
+      <form action="https://google.com" method="POST">
+        <div className="form-step-1 active">
+          <div className="form-close-container">
+            <img src={close} alt="Close button" />
+          </div>
+          <div className="form-title">
+            <span>Processo: </span>
+          </div>
+          <div className="input-flex">
+            <div>
+              <label htmlFor="arrival" className="form-label">
+                {" "}
+                Chegada:
+              </label>
+              <input
+                type="number"
+                name="arrival"
+                placeholder="0"
+                id="arrival"
+                className="form-input"
+                min="0"
+              />
+            </div>
+            <div>
+              <label htmlFor="execution" className="form-label">
+                {" "}
+                Execução:{" "}
+              </label>
+              <input
+                type="number"
+                name="execution"
+                placeholder="1"
+                id="execution"
+                className="form-input"
+              />
+            </div>
+          </div>
+
+          <div className="input-flex">
+            <div>
+              <label htmlFor="dob" className="form-label">
+                {" "}
+                Deadline:{" "}
+              </label>
+              <input
+                type="number"
+                name="deadline"
+                id="deadline"
+                placeholder="0"
+                className="form-input"
+                min="0"
+              />
+            </div>
+            <div>
+              <label htmlFor="numPages" className="form-label">
+                {" "}
+                Páginas:{" "}
+              </label>
+              <input
+                type="number"
+                name="numPages"
+                placeholder="1"
+                id="numPages"
+                className="form-input"
+                min="1"
+              />
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
