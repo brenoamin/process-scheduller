917aefdd74e896ec52fe0796072df96c64c589dd
Breno Amin <brenomatos125@gmail.com>
2023-11-04 15:54:58 -0300

    feature: create the header section

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import "./App.css";
+import { NewButton } from "./components/NewButton";
 import { ProcessBox } from "./components/ProcessBox";
 
 function App() {
-  return <ProcessBox/>;
+  return (
+    <div className="main-section">
+      <header className="background-image">
+        <h1 className="main-section-title"> SCHEDULLER SYSTEM</h1>
+      </header>
+      <div className="process-section">
+        <NewButton />
+        <ProcessBox />
+      </div>
+    </div>
+  );
 }
 
 export default App;
