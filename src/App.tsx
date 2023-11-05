dc080195cd56f6052b8d192136939c58c3e80fd4
Breno Amin <brenomatos125@gmail.com>
2023-11-05 12:58:23 -0300

    feature: create a different div on the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import "./App.css";
+import { DiskMatrix } from "./components/Disk";
 import { NewButton } from "./components/NewButton";
 import { ProcessBox } from "./components/ProcessBox";
+import { RAM } from "./components/RAM/RAM";
 import { SystemSettings } from "./components/SystemSettings";
 
 function App() {
@@ -9,10 +11,17 @@ function App() {
       <header className="background-image">
         <h1 className="main-section-title"> SCHEDULLER SYSTEM</h1>
       </header>
-      <div className="process-section">
-        <SystemSettings/>
-        <NewButton />
-        <ProcessBox />
+      <div>
+        <div className="process-section">
+          <SystemSettings/>
+          <NewButton />
+          <ProcessBox />
+        </div>
+
+        <div className="memory-section">
+          <DiskMatrix />
+          <RAM />
+        </div>
       </div>
     </div>
   );
