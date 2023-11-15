81bba8f112e52ee0fb42ea673d3a191b8910248c
Breno Amin <brenomatos125@gmail.com>
2023-11-15 08:55:59 -0300

    fix: fix css issues on the new button component

--- src/components/NewButton/index.tsx
+++ src/components/NewButton/index.tsx
@@ -5,11 +5,9 @@ import { AddProcessLogo } from "./AddProcessLogo/AddProcessLogo";
 export const NewButton = (): ReactElement => {
   return (
     <div className="new-button-wrapper">
-      <div className="new-button-title">
-        <div className="img-title-wrapper">
-          <AddProcessLogo/>
-          <span>Processo </span>
-        </div>
+      <div className="img-title-wrapper">
+        <AddProcessLogo />
+        <span className="button-title">Processo </span>
       </div>
     </div>
   );
